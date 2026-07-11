8fa6d4aacf52964327a8ce650a14da76c5902342
radiantlegends <radiantlegends@gmail.com>
2026-07-11 16:54:24 -0400

    Hide content until data is loaded

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ setupHints();
 setupPlayers();
 setupAnswers();
 updateHintGiver();
+requestAnimationFrame(() => {
+    document.body.classList.remove("loading");
+});
 
 document.getElementById("new-round-btn").addEventListener("click", skipRound);
 document.getElementById("restart-game-btn").addEventListener("click", resetGame);
